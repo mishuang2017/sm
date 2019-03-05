82e56158a51dc3f3d82d3335fdce10cf96f5e517
chrism <chrism@mellanox.com>
2019-03-05 20:04:22 +0800

    ubuntu bcc

--- ubuntu/setup.sh
+++ ubuntu/setup.sh
@@ -1,3 +1,12 @@
 sudo apt-get install openssh-server vim exuberant-ctags cscope tmux ipcalc	\
 	git make build-essential flex bison libssl-dev libelf-dev		\
 	ethtool tree
+
+# For bionic bcc
+sudo apt-get -y install bison build-essential cmake flex git libedit-dev	\
+	libllvm3.7 llvm-3.7-dev libclang-3.7-dev python zlib1g-dev libelf-dev
+
+# For Lua support
+sudo apt-get -y install luajit luajit-5.1-dev 
+
+sudo apt-get -y netperf iperf iperf3
