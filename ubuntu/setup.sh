a830041aed6149e2e1319400610679a9a0ef111e
Chris Mi <chrism@mellanox.com>
2019-04-02 15:25:16 +0800

    update ubuntu setup.sh

--- ubuntu/setup.sh
+++ ubuntu/setup.sh
@@ -1,4 +1,4 @@
-sudo apt-get install openssh-server vim exuberant-ctags cscope tmux ipcalc	\
+sudo apt-get -y install openssh-server vim exuberant-ctags cscope tmux ipcalc	\
 	git make build-essential flex bison libssl-dev libelf-dev		\
 	ethtool tree linux-tools-generic sysstat
 
@@ -9,7 +9,7 @@ sudo apt-get -y install bison build-essential cmake flex git libedit-dev	\
 # For Lua support
 sudo apt-get -y install luajit luajit-5.1-dev 
 
-sudo apt-get -y netperf iperf iperf3
+sudo apt-get -y install netperf iperf iperf3
 
 # For systemtap
-sudo apt-get -y libdw-dev
+sudo apt-get -y install libdw-dev
