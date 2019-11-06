5f688370a6796e237dbb66523af2e8fc2c790d1b
Chris Mi <chrism@mellanox.com>
2019-11-06 13:33:00 +0800

    buf.py

---
+++ drgn/kernel/buf.py
@@ -0,0 +1,22 @@
+#!/usr/local/bin/drgn -k
+
+from drgn import container_of
+from drgn.helpers.linux import *
+from drgn import Object
+
+import sys
+import os
+import string
+
+libpath = os.path.dirname(os.path.realpath("__file__"))
+sys.path.append(libpath)
+import lib
+
+try:
+    buf = prog['mlx5_buf']
+except LookupError as x:
+    print("no mlx5_buf")
+    sys.exit(1)
+
+for i in str(buf.string_()).split('\\n'):
+    print(i.strip("'"))
