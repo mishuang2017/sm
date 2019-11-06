c26441573247d0722172b083485fe7c9bb82e769
Chris Mi <chrism@mellanox.com>
2019-11-06 14:47:04 +0800

    fix panic in cb

--- drgn/kernel/lib.py
+++ drgn/kernel/lib.py
@@ -6,7 +6,7 @@ import socket
 import subprocess
 import drgn
 prog = drgn.program_from_kernel()
-# prog = drgn.program_from_core_dump("/var/crash/vmcore.1")
+# prog = drgn.program_from_core_dump("/var/crash/vmcore.2")
 
 import os
 
