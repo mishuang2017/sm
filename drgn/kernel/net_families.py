d10a52309fb7cb4c1ebae900d01d34fe322268f1
Chris Mi <chrism@mellanox.com>
2019-12-08 10:23:24 +0800

    update

--- drgn/kernel/net_families.py
+++ drgn/kernel/net_families.py
@@ -19,5 +19,18 @@ NPROTO = 45
 for i in range(NPROTO):
     nf = nfs[i]
     if nf.value_():
-        print("family: %2d, create: %s" % (nf.family.value_(), lib.address_to_name(hex(nf.create))))
-#         print(nf)
+        print("family: %2d, create: %20s" % (nf.family.value_(), lib.address_to_name(hex(nf.create))))
+
+inetsw = prog['inetsw']
+
+SOCK_PACKET = prog['SOCK_PACKET']
+SOCK_MAX = SOCK_PACKET.value_() + 1
+print("")
+
+for i in range(SOCK_MAX):
+    head = inetsw[i]
+    for a in list_for_each_entry('struct inet_protosw', head.address_of_(), 'list'):
+        print("type: %d, protocol: %3d, prot: %15s, ops: %20s" % (a.type.value_(),
+            a.protocol.value_(),
+            lib.address_to_name(hex(a.prot)),
+            lib.address_to_name(hex(a.ops))))
