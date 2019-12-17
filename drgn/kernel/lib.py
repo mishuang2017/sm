e252ff47a3869612722c61544442fc8892662fc9
Chris Mi <chrism@mellanox.com>
2019-12-17 16:05:41 +0800

    merge hash_old and hash_new

--- drgn/kernel/lib.py
+++ drgn/kernel/lib.py
@@ -304,31 +304,6 @@ def struct_exist(name):
         return False
 
 def hash(rhashtable, type, member):
-    if struct_exist("struct rhash_lock_head"):
-        return hash_new(rhashtable, type, member)
-    else:
-        return hash_old(rhashtable, type, member)
-
-def hash_old(rhashtable, type, member):
-    nodes = []
-
-    tbl = rhashtable.tbl
-
-    buckets = tbl.buckets
-    size = tbl.size.value_()
-
-    for i in range(size):
-        rhash_head = buckets[i]
-        while True:
-            if rhash_head.value_() & 1:
-                break;
-            obj = container_of(rhash_head, type, member)
-            nodes.append(obj)
-            rhash_head = rhash_head.next
-
-    return nodes
-
-def hash_new(rhashtable, type, member):
     nodes = []
 
     tbl = rhashtable.tbl
@@ -343,10 +318,11 @@ def hash_new(rhashtable, type, member):
 
     print("")
     for i in range(size):
-        addr = buckets[i]
-        rhash_head = cast("struct rhash_head *", addr)
-        if addr.value_() == 0:
-            continue
+        rhash_head = buckets[i]
+        if struct_exist("struct rhash_lock_head"):
+            rhash_head = cast("struct rhash_head *", rhash_head)
+            if rhash_head.value_() == 0:
+                continue
         while True:
             if rhash_head.value_() & 1:
                 break
