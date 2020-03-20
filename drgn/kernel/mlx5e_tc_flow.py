0db80526e1b7598b55a1c5cbf806202c9a19c643
Chris Mi <chrism@mellanox.com>
2020-03-20 16:15:10 +0800

    print tun_info for mlx5e_tc_flow.py

--- drgn/kernel/mlx5e_tc_flow.py
+++ drgn/kernel/mlx5e_tc_flow.py
@@ -8,11 +8,11 @@ import os
 
 libpath = os.path.dirname(os.path.realpath("__file__"))
 sys.path.append(libpath)
-import lib
+from lib import *
 
-mlx5e_rep_priv = lib.get_mlx5e_rep_priv()
+mlx5e_rep_priv = get_mlx5e_rep_priv()
 
-# if lib.kernel("4.20.16+"):
+# if kernel("4.20.16+"):
 #     tc_ht = mlx5e_rep_priv.uplink_priv.tc_ht
 # else:
 #     tc_ht = mlx5e_rep_priv.tc_ht
@@ -20,16 +20,25 @@ mlx5e_rep_priv = lib.get_mlx5e_rep_priv()
 print("MLX5E_TC_FLOW_FLAG_SIMPLE %x" % (1 << prog['MLX5E_TC_FLOW_FLAG_SIMPLE'].value_()))
 # print("MLX5E_TC_FLOW_FLAG_ESWITCH %x" % 1 << prog['MLX5E_TC_FLOW_FLAG_ESWITCH'].value_())
 
+def print_tun(tun):
+    print("\ttun_info: id: %x, dst ip: %s, dst port: %d" % \
+        (tun.key.tun_id, ipv4(socket.ntohl(tun.key.u.ipv4.dst.value_())), \
+        socket.ntohs(tun.key.tp_dst.value_())))
+
 try:
     prog.type('struct mlx5_rep_uplink_priv')
     tc_ht = mlx5e_rep_priv.uplink_priv.tc_ht
 except LookupError as x:
     tc_ht = mlx5e_rep_priv.tc_ht
 
-for i, flow in enumerate(lib.hash(tc_ht, 'struct mlx5e_tc_flow', 'node')):
+for i, flow in enumerate(hash(tc_ht, 'struct mlx5e_tc_flow', 'node')):
     name = flow.priv.netdev.name.string_().decode()
     print("%-10s mlx5e_tc_flow %lx, cookie: %lx, flags: %x" % \
         (name, flow.value_(), flow.cookie.value_(), flow.flags.value_()))
+
+    tun_info = flow.esw_attr[0].parse_attr.tun_info[0]
+    if tun_info.value_():
+        print_tun(tun_info)
 
 #     continue
 #     print(flow.miniflow_list)
