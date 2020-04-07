496a014f3d43f37082721f6fa810ba107581712e
Chris Mi <chrism@mellanox.com>
2020-04-08 02:09:47 +0300

    fedora_30.sh

---
+++ yum/fedora_30.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+yum install -y rsync ethtool \
+        ctags tmux screen ncurses-devel openssl-devel readline-devel snappy-devel \
+	wget tcl tcl-devel tk tk-devel git-email bc sysstat libglvnd-glx gcc-c++ bison flex make \
+        numactl-devel \	
+        libmnl libmnl-devel \
+        openvswitch libcap-ng-devel libatomic libtool dh-autoreconf \
+        texinfo	\
+        python3-devel dh-autoreconf xz-devel zlib-devel lzo-devel \
+        libnsl \
+        pcre-devel \
+	qalculate \
+        clang clang-devel llvm llvm-devel llvm-static ncurses-devel
+
