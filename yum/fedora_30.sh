d398167ca424e9c2c171b2668525e543ee702bb0
Chris Mi <chrism@mellanox.com>
2020-04-08 22:30:38 +0800

    update

--- yum/fedora_30.sh
+++ yum/fedora_30.sh
@@ -2,7 +2,7 @@
 
 yum update
 yum install -y rsync ethtool \
-        ctags tmux screen ncurses-devel openssl-devel readline-devel snappy-devel \
+        cscope ctags tmux screen ncurses-devel openssl-devel readline-devel snappy-devel \
 	wget tcl tcl-devel tk tk-devel git-email bc sysstat libglvnd-glx gcc-c++ bison flex make \
         numactl-devel \	
         libmnl libmnl-devel \
