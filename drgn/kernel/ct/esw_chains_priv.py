d4eb5ceac5d61fcfaefeb7a362ba396124ec3c92
Chris Mi <chrism@mellanox.com>
2020-04-11 17:00:07 +0800

    update

--- drgn/kernel/ct/esw_chains_priv.py
+++ drgn/kernel/ct/esw_chains_priv.py
@@ -44,9 +44,9 @@ for i, chain in enumerate(hash(chains_ht, 'struct fdb_chain', 'node')):
 def print_mapping_item(item):
     print("mapping_item %lx" % item, end='\t')
     print("cnt: %d" % item.cnt, end='\t')
-    print("id: %d" % item.id, end='\t')
+    print("id (chain_mapping): %d" % item.id, end='\t')
     data = Object(prog, 'int *', address=item.data.address_of_())
-    print("data: 0x%x" % data.value_())
+    print("data (chain): 0x%x" % data.value_())
 
 print('\n=== mapping_ctx ===\n')
 ht = mapping_ctx.ht
