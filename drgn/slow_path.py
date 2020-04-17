e3bd3f6977ac9e41ab9cec9c2681e90c8dcaa367
Chris Mi <chrism@mellanox.com>
2020-04-17 10:47:13 +0800

    print root_ft in flow_path.py

--- drgn/slow_path.py
+++ drgn/slow_path.py
@@ -22,3 +22,16 @@ slow_fdb = mlx5e_priv.mdev.priv.eswitch.fdb_table.offloads.slow_fdb
 flow_table("mlx5e_priv.mdev.priv.eswitch.fdb_table.offloads.slow_fdb", slow_fdb)
 vport_to_tir = mlx5e_priv.mdev.priv.eswitch.offloads.ft_offloads
 flow_table("mlx5e_priv.mdev.priv.eswitch.offloads.ft_offloads", vport_to_tir)
+
+print("\nmlx5e_priv->ppriv(mlx5e_rep_priv)->root_ft")
+for x, dev in enumerate(get_netdevs()):
+    name = dev.name.string_().decode()
+    if pf0_name in name:
+        print(name)
+        mlx5e_priv_addr = dev.value_() + prog.type('struct net_device').size
+        mlx5e_priv = Object(prog, 'struct mlx5e_priv', address=mlx5e_priv_addr)
+        ppriv = mlx5e_priv.ppriv
+        if ppriv:
+            mlx5e_rep_priv = Object(prog, 'struct mlx5e_rep_priv', address=ppriv.value_())
+            print("mlx5_flow_table %lx" % mlx5e_rep_priv.root_ft.value_())
+            print_dest(mlx5e_rep_priv.vport_rx_rule.rule[0])
