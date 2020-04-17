3202d22b90e720f66c9d53bdbd6012c3f4ddeca9
Chris Mi <chrism@mellanox.com>
2020-04-17 15:59:35 +0800

    update

--- drgn/ct/zone_ht.py
+++ drgn/ct/zone_ht.py
@@ -15,8 +15,8 @@ zones_ht = prog['zones_ht']
 
 def print_flow_offload_tuple(t):
 #     print(t)
-    print("flow_offload_tuple %lx" % t.address_of_())
-    print("\tsrc_v4: %10s" % ipv4(socket.ntohl(t.src_v4.s_addr.value_())), end='\t')
+    print("\t\tflow_offload_tuple %lx" % t.address_of_())
+    print("\t\tsrc_v4: %10s" % ipv4(socket.ntohl(t.src_v4.s_addr.value_())), end='\t')
     print("dst_v4: %10s" % ipv4(socket.ntohl(t.dst_v4.s_addr.value_())), end='\t')
     print("src_port: %6d" % socket.ntohs(t.src_port.value_()), end='\t')
     print("dst_port: %6d" % socket.ntohs(t.dst_port.value_()), end='\t')
@@ -27,12 +27,15 @@ def print_flow_offload_tuple(t):
 
 def print_flow_offload(flow):
     print("\tflow_offload %lx" % flow)
-    print("\t\tflags: %x, timeout: %x" % (flow.flags, flow.timeout), end='\t')
+    print("\t\tnf_conn %lx" % flow.ct)
+    print("\t\tflags: %x, timeout: %x, type: %d" % (flow.flags, flow.timeout, flow.type), end='\t')
     print("(NF_FLOW_SNAT: %x)" % (1 << prog['NF_FLOW_SNAT'].value_()), end=' ')
     print("(NF_FLOW_HW: %x)" % (1 << prog['NF_FLOW_HW'].value_()))
 #     print(flow)
 
 for i, flow_table in enumerate(hash(zones_ht, 'struct tcf_ct_flow_table', 'node')):
+    print("tcf_ct_flow_table %lx" % flow_table)
+    print("nf_flowtable %lx" % flow_table.nf_ft.address_of_())
     nf_ft = flow_table.nf_ft
     zone = flow_table.zone
     print("zone: %d" % zone)
@@ -41,7 +44,6 @@ for i, flow_table in enumerate(hash(zones_ht, 'struct tcf_ct_flow_table', 'node'
 #     print(ft_ht)
     for j, tuple_rhash in enumerate(hash(ft_ht, 'struct flow_offload_tuple_rhash', 'node')):
         tuple = tuple_rhash.tuple
-        print_flow_offload_tuple(tuple)
         if tuple.dir.value_() == 0:
             flow_offload = cast("struct flow_offload *", tuple_rhash)
             print_flow_offload(flow_offload)
@@ -49,3 +51,5 @@ for i, flow_table in enumerate(hash(zones_ht, 'struct tcf_ct_flow_table', 'node'
             flow_offload = Object(prog, 'struct flow_offload', address=tuple_rhash.value_() - \
                 prog.type('struct flow_offload_tuple_rhash').size)
             print_flow_offload(flow_offload.address_of_())
+        print_flow_offload_tuple(tuple)
+    print('')
