dba0a0bb2e949f7ba4267de05276fbd8e10762a9
Chris Mi <chrism@mellanox.com>
2020-04-17 14:43:24 +0800

    update

--- drgn/ct/mlx5_tc_ct_priv.py
+++ drgn/ct/mlx5_tc_ct_priv.py
@@ -48,6 +48,15 @@ for node in radix_tree_for_each(fte_ids.idr_rt):
 
 ###############################
 
+tuple_ids = ct_priv.tuple_ids
+
+print("=== mlx5e_rep_priv.uplink_priv.ct_priv.tuple_ids ===")
+for node in radix_tree_for_each(tuple_ids.idr_rt):
+    mlx5_ct_zone_rule = Object(prog, 'struct mlx5_ct_zone_rule', address=node[1].value_())
+    print("tupleid: %x" % mlx5_ct_zone_rule.tupleid)
+
+################################
+
 # print(ct_priv)
 print("\n=== mlx5e_rep_priv.uplink_priv.ct_priv.zone_ht ===")
 zone_ht = ct_priv.zone_ht
@@ -57,15 +66,20 @@ for i, mlx5_ct_ft in enumerate(hash(zone_ht, 'struct mlx5_ct_ft', 'node')):
     print("mlx5_ct_ft %lx" % mlx5_ct_ft)
     print("zone: %d" % mlx5_ct_ft.zone)
 
+    print("nf_flowtable %lx" % mlx5_ct_ft.nf_ft)
+    print("mlx5_ct_ft.ct_entries_ht:")
     ct_entries_ht = mlx5_ct_ft.ct_entries_ht
     for j, mlx5_ct_entry in enumerate(hash(ct_entries_ht, 'struct mlx5_ct_entry', 'node')):
         print("mlx5_ct_entry %lx" % mlx5_ct_entry)
         print("\tzone: %d" % mlx5_ct_entry.zone)
         print("\tcookie is flow_offload_tuple %lx" % mlx5_ct_entry.cookie)
         print("\trestor_cookie is 'ct | ctinfo' %lx" % mlx5_ct_entry.restore_cookie)
+        print('')
 
         for k in range(2):
             mlx5_ct_zone_rule = mlx5_ct_entry.zone_rules[k]
+            print("\tmlx5_ct_entry.zone_rules[%d]" % k)
+            print("\tmlx5_ct_zone_rule %lx" % mlx5_ct_zone_rule.address_of_())
             print("\tmlx5_esw_flow_attr %lx" % mlx5_ct_zone_rule.attr.address_of_())
             mlx5_flow_handle = mlx5_ct_zone_rule.rule
             nat = mlx5_ct_zone_rule.nat
