52b217ef3d731655a7ec30862db81db068ae8bc0
Chris Mi <chrism@mellanox.com>
2020-05-03 10:43:23 +0800

    update

--- drgn/ovs/netdev_classes.py
+++ drgn/ovs/netdev_classes.py
@@ -16,19 +16,27 @@ from lib_ovs import *
 netdev_classes = prog['netdev_classes']
 print(netdev_classes.impl.p)
 
-buckets = netdev_classes.impl.p.buckets
+cmap_impl = netdev_classes.impl.p
+mask = cmap_impl.mask.value_()
+print("mask: %d" % mask)
+
+buckets = cmap_impl.buckets
 
 CMAP_K = 5
 
-for i in range(CMAP_K):
-    print(buckets[0].nodes[i])
-    cmap_node = buckets[0].nodes[i].next.p[0]
-    if cmap_node.address_of_().value_() == 0:
-        continue
-    print(cmap_node)
-    data = container_of(cmap_node.address_of_(), "struct netdev_registered_class", "cmap_node")
-    print(data.member_("class").type)
+n = 0
+for i in range(mask + 1):
+    for j in range(CMAP_K):
+        cmap_node = buckets[i].nodes[j].next.p[0]
+        if cmap_node.address_of_().value_() == 0:
+            continue
+#         print(cmap_node)
+        data = container_of(cmap_node.address_of_(), "struct netdev_registered_class", "cmap_node")
+        print(data.member_("class").type)
+        n += 1
 
-for i in ('netdev_linux_class', 'netdev_internal_class'):
-    new_class = prog[i]
-    print("%10s, netdev_class: %lx" % (new_class.type, new_class.address_of_().value_()))
+print("n = %d" % n)
+
+# for i in ('netdev_linux_class', 'netdev_internal_class'):
+#     new_class = prog[i]
+#     print("%10s, netdev_class: %lx" % (new_class.type, new_class.address_of_().value_()))
