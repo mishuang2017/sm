295c3f66282adb2c5bf86527c3f3481008ca0280
Chris Mi <chrism@mellanox.com>
2020-05-11 14:30:03 +0800

    print udpif_key

--- drgn/ovs/udpif.py
+++ drgn/ovs/udpif.py
@@ -16,17 +16,18 @@ from lib_ovs import *
 backer = get_backer()
 udpif = backer.udpif
 
-n1 = udpif.n_handlers
-n2 = udpif.n_revalidators
+def print_handlers():
+    n1 = udpif.n_handlers
+    n2 = udpif.n_revalidators
 
-rev = udpif.revalidators
+    rev = udpif.revalidators
 
-for i in range(n1):
-#     print("udpif.handler_id: %d" % udpif.handlers[i].handler_id)
-    print(udpif.handlers[i])
+    for i in range(n1):
+    #     print("udpif.handler_id: %d" % udpif.handlers[i].handler_id)
+        print(udpif.handlers[i])
 
-for i in range(n2):
-    print(rev[i])
+    for i in range(n2):
+        print(rev[i])
 
 # dump = udpif.dump
 # print(dump)
@@ -42,3 +43,21 @@ n_revalidators = prog['n_revalidators']
 print("n_revalidators: %d" %n_revalidators)
 n_handlers = prog['n_handlers']
 print("n_handlers: %d" % n_handlers)
+
+# print_handlers()
+
+ukeys = udpif.ukeys
+print(ukeys)
+
+N_UMAPS = 512
+
+for i in range(N_UMAPS):
+    cmap = ukeys[i].cmap
+    if cmap.impl.p.value_() != prog['empty_cmap'].address_of_().value_():
+        print(i)
+        print(cmap)
+        keys = print_cmap(cmap, "udpif_key", "cmap_node")
+        for j, key in enumerate(keys):
+            print(key)
+
+# print(prog['empty_cmap'].address_of_())
