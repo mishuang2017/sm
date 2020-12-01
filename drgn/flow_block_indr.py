667ff505696887a08e8c35a4d643c010f6c3f4d0
Chris Mi <cmi@nvidia.com>
2020-12-01 20:04:08 +0800

    flow_block_indr.py

---
+++ drgn/flow_block_indr.py
@@ -0,0 +1,36 @@
+#!/usr/local/bin/drgn -k
+
+from drgn.helpers.linux import *
+from drgn import Object
+import time
+import sys
+import os
+
+libpath = os.path.dirname(os.path.realpath("__file__"))
+sys.path.append(libpath)
+import lib
+
+mlx5e_priv = lib.get_mlx5_pf0()
+mlx5e_rep_priv = mlx5e_priv.ppriv
+
+def print_neigh_hash_entry(n):
+    print(n)
+#     print("mlx5e_neigh.dst_ip: %s" % n.m_neigh.dst_ip.v6.in6_u.u6_addr8)
+
+def print_encap(e):
+    print(e)
+#     print("encap_id: %d" % e.encap_id)
+
+flow_block_indr_dev_list = prog['flow_block_indr_dev_list']
+
+for flow_indr_dev in list_for_each_entry('struct flow_indr_dev', flow_block_indr_dev_list.address_of_(), 'list'):
+    print(flow_indr_dev)
+    cb_priv = flow_indr_dev.cb_priv
+    mlx5e_rep_priv = Object(prog, 'struct mlx5e_rep_priv', address=cb_priv.value_())
+#     print(mlx5e_rep_priv)
+
+
+flow_block_indr_list = prog['flow_block_indr_list']
+
+for flow_block_cb in list_for_each_entry('struct flow_block_cb', flow_block_indr_list.address_of_(), 'indr.list'):
+    print(flow_block_cb)
