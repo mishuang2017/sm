4cf9d96fd96a4c980f5f3be062c7b87cdb2148d8
Chris Mi <cmi@nvidia.com>
2020-12-17 01:11:49 -0500

    update

--- drgn/4.19.36-bd/mlx5e_tc_flow.py
+++ drgn/4.19.36-bd/mlx5e_tc_flow.py
@@ -11,22 +11,16 @@ from lib import *
 
 mlx5e_rep_priv = get_mlx5e_rep_priv()
 
-# if kernel("4.20.16+"):
-#     tc_ht = mlx5e_rep_priv.uplink_priv.tc_ht
-# else:
-#     tc_ht = mlx5e_rep_priv.tc_ht
-
-# print("MLX5E_TC_FLOW_FLAG_INGRESS   %10x" % (1 << prog['MLX5E_TC_FLOW_FLAG_INGRESS'].value_()))
-# print("MLX5E_TC_FLOW_FLAG_ESWITCH   %10x" % (1 << prog['MLX5E_TC_FLOW_FLAG_ESWITCH'].value_()))
-# print("MLX5E_TC_FLOW_FLAG_OFFLOADED %10x" % (1 << prog['MLX5E_TC_FLOW_FLAG_OFFLOADED'].value_()))
-# print("MLX5E_TC_FLOW_FLAG_SIMPLE    %10x" % (1 << prog['MLX5E_TC_FLOW_FLAG_SIMPLE'].value_()))
-# print("MLX5E_TC_FLOW_FLAG_CT        %10x" % (1 << prog['MLX5E_TC_FLOW_FLAG_CT'].value_()))
-# print("MLX5E_TC_FLOW_FLAG_CT_ORIG   %10x" % (1 << prog['MLX5E_TC_FLOW_FLAG_CT_ORIG'].value_()))
+print("MLX5E_TC_FLOW_INGRESS   %10x" % (prog['MLX5E_TC_FLOW_INGRESS'].value_()))
+print("MLX5E_TC_FLOW_ESWITCH   %10x" % (prog['MLX5E_TC_FLOW_ESWITCH'].value_()))
+print("MLX5E_TC_FLOW_OFFLOADED %10x" % (prog['MLX5E_TC_FLOW_OFFLOADED'].value_()))
+print("MLX5E_TC_FLOW_SIMPLE    %10x" % (prog['MLX5E_TC_FLOW_SIMPLE'].value_()))
+print("MLX5E_TC_FLOW_CT        %10x" % (prog['MLX5E_TC_FLOW_CT'].value_()))
 
 print('')
-# print("MLX5_MATCH_OUTER_HEADERS     %10x" % prog['MLX5_MATCH_OUTER_HEADERS'].value_())
-# print("MLX5_MATCH_MISC_PARAMETERS   %10x" % prog['MLX5_MATCH_MISC_PARAMETERS'].value_())
-# print("MLX5_MATCH_MISC_PARAMETERS_2 %10x" % prog['MLX5_MATCH_MISC_PARAMETERS_2'].value_())
+print("MLX5_MATCH_OUTER_HEADERS     %10x" % prog['MLX5_MATCH_OUTER_HEADERS'].value_())
+print("MLX5_MATCH_MISC_PARAMETERS   %10x" % prog['MLX5_MATCH_MISC_PARAMETERS'].value_())
+print("MLX5_MATCH_MISC_PARAMETERS_2 %10x" % prog['MLX5_MATCH_MISC_PARAMETERS_2'].value_())
 
 try:
     prog.type('struct mlx5_rep_uplink_priv')
@@ -40,15 +34,10 @@ for i, flow in enumerate(hash(tc_ht, 'struct mlx5e_tc_flow', 'node')):
         (name, flow.value_(), flow.cookie.value_(), flow.flags.counter.value_(), flow.refcnt.refs.counter))
     print("chain: %x" % flow.esw_attr[0].chain, end='\t')
     print("dest_chain: %x" % flow.esw_attr[0].dest_chain, end='\t')
-#     print("fdb: %x" % flow.esw_attr[0].fdb, end='\t')
-#     print("dest_ft: %x" % flow.esw_attr[0].dest_ft, end='\t')
 #     print("ct_state: %x/%x" % (flow.esw_attr[0].parse_attr.spec.match_value[57] >> 8, \
 #         flow.esw_attr[0].parse_attr.spec.match_criteria[57] >> 8))
     print("mlx5_flow_spec %lx" % flow.esw_attr[0].parse_attr.spec.address_of_())
-#     print("sample_rate: %x" % flow.esw_attr[0].sample_rate)
-#     print("psample_group_num: %x" % flow.esw_attr[0].psample_group_num)
     print("action: %x" % flow.esw_attr[0].action)
-#     print(flow.sampler)
 #     print("match_criteria_enable: %x" % flow.esw_attr[0].parse_attr.spec.match_criteria_enable)
 #     print(flow.esw_attr[0].parse_attr)
 #     print(flow.esw_attr[0])
