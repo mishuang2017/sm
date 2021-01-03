10295e08cf595d6f13bd45f63da63d203b2b7021
Chris Mi <cmi@nvidia.com>
2021-01-03 17:49:57 +0800

    move print_vxlan_udphdr(x) and print_mlx5e_encap_entry(e) to lib

--- drgn/lib.py
+++ drgn/lib.py
@@ -914,3 +914,37 @@ def get_pcpu_refcnt(dev):
 #         print(pointer)
         count += refcnt
     return count
+
+def print_vxlan_udphdr(x):
+    print("dest mac          : %x:%x:%x:%x:%x:%x" % (x[0], x[1], x[2], x[3], x[4], x[5]))
+    print("src mac           : %x:%x:%x:%x:%x:%x" % (x[6], x[7], x[8], x[9], x[10], x[11]))
+    print("ethertype         : 0x%02x%02x" % (x[12], x[13]))
+    print("version|IHL       : 0x%02x" % (x[14]))
+    print("DSCP|ECN          : 0x%02x" % (x[15]))
+    print("total length      : 0x%02x%02x" % (x[16], x[17]))
+    print("identification    : 0x%02x%02x" % (x[18], x[19]))
+    print("flags|frag offset : 0x%02x%02x" % (x[20], x[21]))
+    print("tme to live       : 0x%02x" % (x[22]))
+    print("protocol          : 0x%02x" % (x[23]))
+    print("header checksum   : 0x%02x%02x" % (x[24], x[25]))
+    print("source IP address : %d.%d.%d.%d" % (x[26], x[27], x[28], x[29]))
+    print("  dest IP address : %d.%d.%d.%d" % (x[30], x[31], x[32], x[33]))
+    print("UDP source port   : %d" % (x[34] << 8 | x[35]))
+    print("UDP dest port     : %d" % (x[36] << 8 | x[37]))
+    print("UDP length        : %d" % (x[38] << 8 | x[39]))
+    print("UDP checksum      : %02x%02x" % (x[40], x[41]))
+    print("vxlan(VNI present): %02x%02x%02x%02x" % (x[42], x[43], x[44], x[45]))
+    print("vxlan(VNI)        : %d" % (x[46] << 16 | x[47] << 8 | x[48]))
+    print("vxlan(reserved)   : %d" % (x[49]))
+
+def print_mlx5e_encap_entry(e):
+#     print(e)
+    print(e.encap_header)
+    x = Object(prog, 'unsigned char *', address=e.encap_header.address_of_())
+    print_vxlan_udphdr(x)
+    print("encap_size: %d" % e.encap_size)
+#     for i in range(e.encap_size):
+#         print("%#x " % e.encap_header[i])
+    print("mlx5e_encap_entry %lx" % e.value_())
+    print(e.m_neigh)
+    print_tun(e.tun_info)
