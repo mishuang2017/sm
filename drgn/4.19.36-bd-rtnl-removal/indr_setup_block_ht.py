e999058b38705b347e1f9de76ba98823e04715a7
Chris Mi <cmi@nvidia.com>
2021-01-05 00:29:41 -0500

    ndr_setup_block_ht.py

---
+++ drgn/4.19.36-bd-rtnl-removal/indr_setup_block_ht.py
@@ -0,0 +1,31 @@
+#!/usr/local/bin/drgn -k
+
+from drgn.helpers.linux import *
+from drgn import Object
+from drgn import container_of
+from drgn import cast
+import time
+import socket
+import sys
+import os
+
+sys.path.append("..")
+from lib import *
+
+indr_setup_block_ht = prog['indr_setup_block_ht']
+# print(indr_setup_block_ht)
+
+def print_indr_setup_block_ht(block):
+    print(block)
+    cb_list = block.cb_list
+#     print(cb_list)
+    for e in list_for_each_entry('struct flow_indr_block_cb', cb_list.address_of_(), 'list'):
+#         print(e)
+        print(address_to_name(hex(e.cb)))
+        priv = Object(prog, "struct mlx5e_rep_priv", address=e.cb_priv.address_of_())
+#         print(priv)
+        print("mlx5e_rep_priv %lx" % e.cb_priv.address_of_())
+
+for i, node in enumerate(hash(indr_setup_block_ht, 'struct flow_indr_block_dev', 'ht_node')):
+    print(i)
+    print_indr_setup_block_ht(node)
